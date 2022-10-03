160eec7532c5e08b63ef2303eb6089c3413b9e8a
Rebecca C <20203tn077@utez.edu.mx>
2022-10-02 23:09:34 -0500

    Corrección de estructura del repositorio 2

---
+++ libs/view.php
@@ -0,0 +1,13 @@
+<?php
+class View
+{
+    function __construct()
+    {
+        // echo "view template...";
+    }
+
+    function render($viewname)
+    {
+        require "views/$viewname.php";
+    }
+}
--- myapp/controllers/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Errors
-{
-    function __construct()
-    {
-        echo "<p>Error loading requested resource...</p>";
-    }
-}
